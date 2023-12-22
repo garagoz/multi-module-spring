f3c8a8ab7092c16983abc9f80943ffb102b3b943
arslan <atorayew@gmail.com>
2023-12-22 10:17:12 +0500

    person service class modified

--- spring-datajpa-postgresql/src/main/java/com/garagoz/service/PersonService.java
+++ spring-datajpa-postgresql/src/main/java/com/garagoz/service/PersonService.java
@@ -1,4 +1,13 @@
 package com.garagoz.service;
 
-public class PersonService {
+import com.garagoz.dto.PersonDto;
+import org.springframework.data.domain.Page;
+
+import java.awt.print.Pageable;
+
+public interface PersonService {
+    PersonDto save(PersonDto personDto);
+    void delete(Long id);
+    PersonDto getAll(PersonDto personDto);
+    Page<PersonDto> getAll(Pageable pageable);
 }
